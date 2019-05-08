bfdd63bceca810d829c80bfe57c237c8a16c66c1
Rémi Salmi <remi.salmi@gmail.com>
2019-05-08 13:39:44 +0200

    add public folder

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
+//Init express
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const path = require('path');
+
+//Init bp
+const bodyParser = require('body-parser');
 
 //Routes imports
 const utilisateursRoute = require('./routes/utilisateursRoute');
 const exercicesRoute = require('./routes/exercicesRoute');
 const programmesRoute = require('./routes/programmesRoute');
 
-const port = 3000;
+app.use('/utilisateurs', utilisateursRoute);
+app.use('/exercices', exercicesRoute);
+app.use('/programmes', programmesRoute);
 
-// Dossier des assets
+//Port to use
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+//Assets's folder
 app.use('/public', express.static('public'));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-
-//Routes imports
-app.use('/utilisateurs', utilisateursRoute);
-app.use('/exercices', exercicesRoute);
-app.use('/programmes', programmesRoute);
-
+//Index route
 app.get('/', (req, res) => {
-    res.render('index');
+    res.redirect('/exercices');
 });
 
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
