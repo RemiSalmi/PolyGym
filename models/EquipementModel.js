1cbf22205303f1f3abb7e7a5c815a13410b16f84
Rémi Salmi <remi.salmi@gmail.com>
2019-05-11 14:18:08 +0200

    fix db connexion

--- models/EquipementModel.js
+++ models/EquipementModel.js
@@ -1,5 +1,5 @@
-const { Client } = require('pg')
-const pool = new Client({
+const { Pool } = require('pg')
+const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
 })
