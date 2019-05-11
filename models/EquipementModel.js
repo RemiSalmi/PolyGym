30b522fe26803196ea8d6d44602b67b8dcaccda8
Rémi Salmi <remi.salmi@gmail.com>
2019-05-11 13:32:52 +0200

    fix db connection to deploying

--- models/EquipementModel.js
+++ models/EquipementModel.js
@@ -1,5 +1,12 @@
 const { Pool } = require('pg')
-const pool = new Pool()
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DB,
+    password: process.env.DB_PWD,
+    port: process.env.DB_PORT,
+    ssl: true
+})
 
 
 class Equipement {
