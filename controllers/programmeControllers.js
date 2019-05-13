17d2622692f66c35c5258f2881266848813d9116
Rémi Salmi <remi.salmi@gmail.com>
2019-05-13 19:22:00 +0200

    add read function

--- controllers/programmeControllers.js
+++ controllers/programmeControllers.js
@@ -1,15 +1,33 @@
 const Programme = require('../models/ProgrammeModel');
 
+//Init jwt
+var jwt = require('jsonwebtoken');
+
 exports.create = (req,res) =>{
 
 }
 
 exports.read = (req,res) =>{
+    const id = parseInt(req.params.id)
+    Programme.getExercicesByProg(id)
+    .then(exs => {
+        res.render('programme', {title: 'Programme',exs})
+    })
+    .catch(err =>{
+        console.error(err)
+    })
 
 }
 
 exports.readAll = (req,res) =>{
-
+    const tokenInfo = jwt.decode(req.cookies.token)
+    Programme.getAllByUser(tokenInfo.userId)
+    .then(tabProg =>{
+        res.render('mesProgrammes', {title: 'Mes programmes', tabProg})
+    })
+    .catch(err => {
+        console.log(err)
+    })
 }
 
 exports.update = (req,res) =>{
