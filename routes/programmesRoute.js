0fc82b5dc64c502fd75aab6ae818a36ffbf1c3d4
Rémi Salmi <remi.salmi@gmail.com>
2019-05-13 19:55:24 +0200

    use programmeMiddleware

--- routes/programmesRoute.js
+++ routes/programmesRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Auth = require('../middlewares/Auth')
+const progCheck = require('../middlewares/programmeMiddleware')
 
 const programController = require('../controllers/programmeControllers');
 
@@ -9,7 +10,7 @@ const programController = require('../controllers/programmeControllers');
 router.get('/mesProgrammes',Auth.isConnected, programController.readAll)
 
 //Consult the detail of a program
-router.get('/mesProgrammes/:id',Auth.isConnected, programController.read)
+router.get('/mesProgrammes/:id',Auth.isConnected, progCheck.isMyProg , programController.read)
 
 //Update a program
 router.put('/mesProgrammes/:id',Auth.isConnected, programController.update)
