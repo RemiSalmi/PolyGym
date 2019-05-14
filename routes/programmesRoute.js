b5001cd326be4b25aa099a63e4dcd775ae6d900c
Rémi Salmi <remi.salmi@gmail.com>
2019-05-14 09:49:44 +0200

    add route to acces to the creerProg page

--- routes/programmesRoute.js
+++ routes/programmesRoute.js
@@ -4,21 +4,28 @@ const router = express.Router();
 const Auth = require('../middlewares/Auth')
 const progCheck = require('../middlewares/programmeMiddleware')
 
+//Init bp
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false})
+
 const programController = require('../controllers/programmeControllers');
 
 //Consult all our own programs
 router.get('/mesProgrammes',Auth.isConnected, programController.readAll)
 
-//Consult the detail of a program
+//Get the page "create program"
+router.get('/mesProgrammes/creer',Auth.isConnected,programController.getCreatePage)
+
+//Consult the detail of one of our programs
 router.get('/mesProgrammes/:id',Auth.isConnected, progCheck.isMyProg , programController.read)
 
-//Update a program
+//Update one of our programs
 router.put('/mesProgrammes/:id',Auth.isConnected, programController.update)
 
-//Delet a program
+//Delete one of our programs
 router.delete('/mesProgrammes/:id',Auth.isConnected, programController.delete)
 
-//Create a program
-router.post('/',Auth.isConnected, programController.create)
+//Create a personal program
+router.post('/mesProgrammes',urlencodedParser,Auth.isConnected, programController.create)
 
 module.exports = router;
