f332949ec14d0a1f1f096b7ce3d91c2cf9553cfe
Rémi Salmi <remi.salmi@gmail.com>
2019-05-14 16:47:15 +0200

    add a security middleware

--- middlewares/programmeMiddleware.js
+++ middlewares/programmeMiddleware.js
@@ -4,7 +4,7 @@ const Utilisateur = require('../models/UtilisateurModel')
 const jwt = require('jsonwebtoken')
 
 exports.isMyProg = (req, res, next) => {
-    idUtilisateur = jwt.decode(req.cookies.token).userId
+    const idUtilisateur = jwt.decode(req.cookies.token).userId
     Utilisateur.checkProgram(idUtilisateur, req.params.id)
     .then(resulat =>{
         if (resulat.rowCount == 0){
@@ -13,4 +13,19 @@ exports.isMyProg = (req, res, next) => {
             next()
         }
     } )
+}
+
+exports.VerifProgOwner = (req, res, next) => {
+    const idProg = req.body.inputIdProg
+    const idUtilisateur = jwt.decode(req.cookies.token).userId
+    console.log(idProg)
+    Utilisateur.checkProgram(idUtilisateur, idProg)
+    .then(resulat =>{
+        if (resulat.rowCount == 0){
+            res.status(401).send({error: 'Vous essayez d\'accéder à un programme qui n\'est pas le vôtre'})
+        }else{
+            next()
+        }
+    } )
+
 }
