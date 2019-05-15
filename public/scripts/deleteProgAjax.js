5719603758e1ee90cbdb425b49f1200b9ae0e78c
Rémi Salmi <remi.salmi@gmail.com>
2019-05-15 14:36:17 +0200

    add new script

---
+++ public/scripts/deleteProgAjax.js
@@ -0,0 +1,26 @@
+{
+    let btns = document.getElementsByClassName('deleteBtn');
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", deleteProg)
+    }
+
+    function deleteProg(event) {
+        const idProg = event.target.parentNode.children[0].innerHTML
+
+        console.log(idProg)
+
+        let Request = new XMLHttpRequest()
+        const address = '/programmes/mesProgrammes/' + idProg
+        Request.open("DELETE", address, true)
+        Request.onreadystatechange = function () {
+            if (Request.readyState == XMLHttpRequest.DONE && Request.status == 200) {
+                window.location.reload()
+            }
+            if (Request.readyState == XMLHttpRequest.DONE && Request.status == 401) {
+                document.getElementById('error').innerHTML = JSON.parse(Request.response).error + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
+                document.getElementById('error').style.display = "block"
+            }
+        }
+        Request.send()
+    }
+}
